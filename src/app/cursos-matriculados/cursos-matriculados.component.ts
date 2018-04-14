ac0ad04cb65d1824db9c78c0b1e6f0cfa8b4ca42
Ma-lt <mlucio.alt@gmail.com>
2018-04-13 20:33:58 -0600

    Added visualizacion de cursos

--- src/app/cursos-matriculados/cursos-matriculados.component.ts
+++ src/app/cursos-matriculados/cursos-matriculados.component.ts
@@ -21,20 +21,49 @@ export class CursosMatriculadosComponent implements OnInit {
     private route: ActivatedRoute, private router: Router) { }
 
     private estudiante: Estudiante;
+    private estudianteId: string;
     private institucion: string;
+    private cursos: Array<Curso>;
+    private cursosMatriculados: Array<Curso> = [];
 
   ngOnInit() {
       let usr = this.route.snapshot.parent.paramMap.get('usr');
       this.adminEstService.getEstudiante(usr).subscribe(
         data => {
             this.estudiante = data;
+            this.estudianteId = data._id;
             this.setInstitucion(data.institucion);
-            console.log(data);
+            this.getCursos();
             })
   }
 
   setInstitucion(institucion){
       this.institucion = institucion._id;
   }
+
+    getCursos(){
+      this.adminCurService.getCursos(this.institucion).subscribe( data => {
+          this.cursos = data;
+          //  console.log(data);
+              this.getGrupos();
+      });
+    }
+
+    getGrupos(){
+        for(let curso of this.cursos){
+            for(let grupo of curso.grupos){
+                for(let estudiante of grupo.estudiantes){
+                    var json = JSON.stringify(estudiante);
+                    json = json.replace(/['"]+/g, '');
+                    if(json == this.estudianteId){
+                        this.cursosMatriculados.push(curso);
+                        }
+                    }
+                }
+
+            }
+        }
+            
+
 
 }
